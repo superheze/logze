3cde611c342a9be084784db6d5a64df51dde6f93
heze <heze@staff.sina.com.cn>
2022-01-06 16:58:00 +0800

    add LogLevel struct

---
+++ level.go
@@ -0,0 +1,33 @@
+package logze
+
+type LogLevel int8
+
+const (
+	PanicLevel LogLevel = iota
+	FatalLevel
+	ErrorLevel
+	WarningLevel
+	InfoLevel
+	DebugLevel
+	TraceLevel
+)
+
+func (logLevel LogLevel) String() string {
+	switch logLevel {
+	case TraceLevel:
+		return "trace"
+	case DebugLevel:
+		return "debug"
+	case InfoLevel:
+		return "info"
+	case WarningLevel:
+		return "warning"
+	case ErrorLevel:
+		return "error"
+	case FatalLevel:
+		return "fatal"
+	case PanicLevel:
+		return "panic"
+	}
+	return "unknown"
+}
